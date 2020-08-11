ef1244d1c5900d9d77f33351371605deba5e6683
chenzijie <chenzijie@weidian.com>
2020-08-11 15:59:23 +0800

    Initial Commit

---
+++ opengl-es/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  opengl-es
+//
+//  Created by blackox626 on 2020/8/11.
+//  Copyright © 2020 vdian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ opengl-es/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  opengl-es
+//
+//  Created by blackox626 on 2020/8/11.
+//  Copyright © 2020 vdian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
